af5e9b928053d63a2f4326a9e1850d0a929f8d04
Kauan Sena <lkrs@protonmail.com>
2024-02-21 21:39:05 -0300

    Update duckgo_engine.py

--- duckgo_engine.py
+++ duckgo_engine.py
@@ -8,11 +8,15 @@ import requests
 # https://duckduckgo.com/?q= {PESQUISA} &ia=web
 # https://duckduckgo.com/?t=h_&q= {PESQUISA} &ia=web
 
-query =  'python'
-url = f'https://duckduckgo.com/?q={query}'
+query = 'python'
+url = f'https://duckduckgo.com/?q={query}&ia=web'
 
-req = requests.get(url)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
+
+req = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(req.text, 'html.parser')
+
+elementos = soup.find_all("span", class_="EKtkFWMYpwzMKOYr0GYm LQVY1Jpkk8nyJ6HBWKAk")
 
 print(soup)
